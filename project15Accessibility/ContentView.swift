6eab7ef5a69dea71e285eff86fb8d618dc6ae24e
BingyuanXu <xubingyuan123@hotmail.com>
2020-08-01 21:26:55 -0500

    basic

--- project15Accessibility/ContentView.swift
+++ project15Accessibility/ContentView.swift
@@ -9,13 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  
+  let pictures = [
+    "ales-krivec-15949",
+    "galina-n-189483",
+    "kevin-horstmann-141705",
+    "nicolas-tissot-335096"
+  ]
+  
+  @State private var selectedPicture = Int.random(in: 0...3)
+  
+  var body: some View {
+    Image(pictures[selectedPicture])
+    .resizable()
+    .scaledToFit()
+      .onTapGesture {
+        self.selectedPicture = Int.random(in: 0...3)
     }
+  }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
